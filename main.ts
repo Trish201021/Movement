ea27dc9ebfbabf68d324c5ad988cb69b4377027a
Trish201021 <100299339+Trish201021@users.noreply.github.com>
2022-04-12 10:31:35 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        SPRITE.move(-1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        SPRITE.move(1)
+        SPRITE.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        SPRITE.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        SPRITE.move(-1)
+        SPRITE.change(LedSpriteProperty.Y, 1)
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        SPRITE.change(LedSpriteProperty.Y, -1)
+        basic.pause(500)
+    }
+})
+let SPRITE: game.LedSprite = null
+SPRITE = game.createSprite(4, 0)
+basic.forever(function () {
+	
+})
