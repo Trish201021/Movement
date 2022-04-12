0111409be3bb69f337d35a48e497c4c9f51f0a95
Trish201021 <100299339+Trish201021@users.noreply.github.com>
2022-04-12 10:48:34 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        SPRITE.move(-1)
-        basic.pause(500)
-    }
-    for (let index = 0; index < 4; index++) {
-        SPRITE.move(1)
-        SPRITE.change(LedSpriteProperty.Y, 1)
-        basic.pause(500)
-    }
-    for (let index = 0; index < 4; index++) {
-        SPRITE.change(LedSpriteProperty.Y, -1)
-        basic.pause(500)
-    }
-    for (let index = 0; index < 4; index++) {
-        SPRITE.move(-1)
-        SPRITE.change(LedSpriteProperty.Y, 1)
-        basic.pause(500)
-    }
-    for (let index = 0; index < 4; index++) {
-        SPRITE.change(LedSpriteProperty.Y, -1)
+    for (let index = 0; index < 3; index++) {
+        for (let index = 0; index < 4; index++) {
+            SPRITE.move(-1)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            SPRITE.move(1)
+            SPRITE.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            SPRITE.change(LedSpriteProperty.Y, -1)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            SPRITE.move(-1)
+            SPRITE.change(LedSpriteProperty.Y, 1)
+            basic.pause(500)
+        }
+        for (let index = 0; index < 4; index++) {
+            SPRITE.change(LedSpriteProperty.Y, -1)
+            basic.pause(500)
+        }
+        SPRITE.change(LedSpriteProperty.X, 5)
         basic.pause(500)
     }
 })
